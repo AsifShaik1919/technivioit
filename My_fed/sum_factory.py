1693b103c3078d02d7e9f9b9dace7fcbcea35bd4
asif shaik <asif.shaik.56676@gmail.com>
2026-03-02 15:21:44 -0600

    sum_factory.py

---
+++ My_fed/sum_factory.py
@@ -0,0 +1,61 @@
+# Copyright 2020, The TensorFlow Federated Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Factory for summation."""
+
+import typing
+
+import federated_language
+
+from tensorflow_federated.python.aggregators import factory
+from tensorflow_federated.python.common_libs import py_typecheck
+from tensorflow_federated.python.core.templates import aggregation_process
+from tensorflow_federated.python.core.templates import measured_process
+
+
+class SumFactory(factory.UnweightedAggregationFactory):
+  """`UnweightedAggregationFactory` for sum.
+
+  The created `tff.templates.AggregationProcess` sums values placed at
+  `CLIENTS`, and outputs the sum placed at `SERVER`.
+
+  The process has empty `state` and returns no `measurements`. For summation,
+  implementation delegates to the `federated_language.federated_sum` operator.
+  """
+
+  def create(
+      self, value_type: factory.ValueType
+  ) -> aggregation_process.AggregationProcess:
+    type_args = typing.get_args(factory.ValueType)
+    py_typecheck.check_type(value_type, type_args)
+
+    @federated_language.federated_computation()
+    def init_fn():
+      return federated_language.federated_value((), federated_language.SERVER)
+
+    @federated_language.federated_computation(
+        init_fn.type_signature.result,
+        federated_language.FederatedType(
+            value_type, federated_language.CLIENTS
+        ),
+    )
+    def next_fn(state, value):
+      summed_value = federated_language.federated_sum(value)
+      empty_measurements = federated_language.federated_value(
+          (), federated_language.SERVER
+      )
+      return measured_process.MeasuredProcessOutput(
+          state, summed_value, empty_measurements
+      )
+
+    return aggregation_process.AggregationProcess(init_fn, next_fn)
